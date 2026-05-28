2750c5a00a4224de391daaa54a0b6f8f84f4760c
Rafael Albuquerque <labdcircuitos@gmail.com>
2026-05-27 23:38:11 -0300

    feat: prepare Bluetooth foreground service base

---
+++ lablink-android/app/src/main/java/br/com/laboratoriodecircuitos/lablink/core/bluetooth/BluetoothForegroundServiceConfig.kt
@@ -0,0 +1,15 @@
+﻿package br.com.laboratoriodecircuitos.lablink.core.bluetooth
+
+object BluetoothForegroundServiceConfig {
+    const val NOTIFICATION_CHANNEL_ID = "lablink_bluetooth_connection"
+    const val NOTIFICATION_CHANNEL_NAME = "Conexão Bluetooth"
+    const val NOTIFICATION_ID = 1001
+
+    const val ACTION_CONNECT = "br.com.laboratoriodecircuitos.lablink.bluetooth.ACTION_CONNECT"
+    const val ACTION_DISCONNECT = "br.com.laboratoriodecircuitos.lablink.bluetooth.ACTION_DISCONNECT"
+    const val ACTION_SEND_COMMAND = "br.com.laboratoriodecircuitos.lablink.bluetooth.ACTION_SEND_COMMAND"
+
+    const val EXTRA_DEVICE_NAME = "extra_device_name"
+    const val EXTRA_DEVICE_ADDRESS = "extra_device_address"
+    const val EXTRA_COMMAND = "extra_command"
+}
