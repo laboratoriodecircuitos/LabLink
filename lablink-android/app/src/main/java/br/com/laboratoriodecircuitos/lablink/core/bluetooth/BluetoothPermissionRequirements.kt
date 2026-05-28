fec9206ed09e2d63b3744ea1013007f96923050c
Rafael Albuquerque <labdcircuitos@gmail.com>
2026-05-27 22:18:39 -0300

    feat: add Bluetooth discovery permissions

---
+++ lablink-android/app/src/main/java/br/com/laboratoriodecircuitos/lablink/core/bluetooth/BluetoothPermissionRequirements.kt
@@ -0,0 +1,29 @@
+﻿package br.com.laboratoriodecircuitos.lablink.core.bluetooth
+
+import android.Manifest
+import android.os.Build
+
+object BluetoothPermissionRequirements {
+    fun connectionPermissions(): Array<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(Manifest.permission.BLUETOOTH_CONNECT)
+        } else {
+            emptyArray()
+        }
+    }
+
+    fun discoveryPermissions(): Array<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+            )
+        } else {
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    fun allBluetoothRuntimePermissions(): Array<String> {
+        return (connectionPermissions() + discoveryPermissions()).distinct().toTypedArray()
+    }
+}
